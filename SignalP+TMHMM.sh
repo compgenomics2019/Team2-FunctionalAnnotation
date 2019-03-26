0f4dc86d85234d5415079ac1206455654673d70a
Raut, Priyam <priyam.raut@gatech.edu>
2019-03-26 03:26:31 -0400

    Update SignalP+TMHMM.sh

--- SignalP+TMHMM.sh
+++ SignalP+TMHMM.sh
@@ -3,6 +3,8 @@
 for i in ~/input_results/Prodigal_results/proteins/*.faa; do 
  ~/signalp-5.0/bin/signalp -fasta $i -org gram- -format short -gff3
 done
+for f in *.gff3; do mv -- "$f" "signalp_$f" ; done
+
 
 for i in ~/input_files/Prodigal_results/proteins/*.faa; do 
 #echo $i
@@ -14,6 +16,11 @@ basename="${filename%.[^.]*}"
 #echo $basename
 cat $i | ~/tmhmm-2.0c/bin/tmhmm --short > ${basename}.tmhmm
 done
+
+sed -ir 's/^.*PredHel=0.*//' *.tmhmm
+sed -i '/^$/d' *.tmhmm
+for f in *.gff; do mv -- "$f" "tmhmm_$f" ; done
+
 
 
 
