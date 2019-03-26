1558715ebeb72532149dbdede744642a59e57ab5
Gupta, Mansi <mgupta313@gatech.edu>
2019-03-26 00:46:32 -0400

    Update CRT.py

--- CRT.py
+++ CRT.py
@@ -10,7 +10,7 @@ inputDir = sys.argv[1]
 inputfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
 for file1 in inputfiles:
   if "fasta" in file1:
-    ofile = file1.split(".")[0}+"_crt.out"
+    ofile = file1.split(".")[0]+"_crt.out"
     #print(ofile)
     subprocess.call(['java', '-cp', 'CRT1.2-CLI.jar', 'crt', file1, ofile])
 #print(inputfiles)
