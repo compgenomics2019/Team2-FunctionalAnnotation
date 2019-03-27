cb466460a5fba0e0d851f2baae81cfdd20dd9f7d
Cao, Mingming <mcao42@gatech.edu>
2019-03-27 01:16:40 -0400

    Update interpro_annotation.py

--- domain/interpro_annotation.py
+++ domain/interpro_annotation.py
@@ -1,24 +1,13 @@
 #!usr/bin/python 3
 
-import sys 
-from subprocess import Popen, PIPE
+import sys,os
 
 # Usage: interpro_annotation <input_file> <output_file>
 
 def annotation_interproscan(input_file,output_file):
     
     # Apply all database for protein searching
-    logfile1 = open("./logfile_interpro_stdout.log",'w')
-    logfile2 = open("./logfile_interpro_stderr.log",'w')
-    
-    process = Popen(args = ['interproscan.sh',   
-                            '-i',input_file,    
-                            '-f','gff3',     # hmmer method can be replaced
-                            '-o',output_file,'-dp'],
-                            stdout = logfile1, stderr = logfile2)
-    stdout, stderr = process.communicate()
-    
-    del stdout,stderr
+    os.system("/projects/team3/func_annot/bin/interproscan/interproscan-5.33-72.0/interproscan.sh -i " + input_file + "-f gff3 -o " + output_file + "-dp")
 
 def main():
     input_file = sys.argv[1]
