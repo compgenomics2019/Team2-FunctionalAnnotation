9d39fedae630e84766553f92dee1a268dce2c53d
Raut, Priyam <priyam.raut@gatech.edu>
2019-03-26 22:15:47 -0400

    Update signalprun.py

--- sp/signalprun.py
+++ sp/signalprun.py
@@ -4,13 +4,13 @@ import subprocess,os
 
 def signalP_finding():
     outputdir='./signalp'
-    path = '/home/priyam/bin/fannotation/Prodigal_results/proteins'
+    path = '/projects/team2/gene_pred/Prodigal_results/proteins'
     Files = [f for f in os.listdir(path) if f.endswith('.faa')]
     for File in Files:
         print(File)
         Filetemp=path+'/'+File
         print(Filetemp)
-        subprocess.Popen(['/home/priyam/bin/signalp-5.0/bin/signalp','-fasta', Filetemp, '-org','gram-','-format','short','-gff3']).communicate()
+        subprocess.Popen(['/projects/team2/func_annotation/tools/signalp-5.0/bin/signalp','-fasta', Filetemp, '-org','gram-','-format','short','-gff3']).communicate()
     subprocess.call(['mkdir',outputdir])
     gff3Files = [f for f in os.listdir(path) if f.endswith('.gff3')]
     signalp5Files= [f for f in os.listdir(path) if f.endswith('.signalp5')]
