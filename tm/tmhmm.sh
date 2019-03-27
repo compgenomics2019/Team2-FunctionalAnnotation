1a9d806ae0d3e7e231f9acc9c68d99a08571725b
Huang, Tzu-Chuan <tc_huang@gatech.edu>
2019-03-27 04:05:29 -0400

    Update tmhmm.sh

--- tm/tmhmm.sh
+++ tm/tmhmm.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-path = $1
 
 for i in $1/*.faa; do 
 #echo $i
@@ -13,12 +12,12 @@ basename="${filename%.[^.]*}"
 cat $i | /projects/team2/func_annotation/tools/tmhmm-2.0c/bin/tmhmm --short > ${basename}.tmhmm
 done
 
-sed -ir 's/^.*PredHel=0.*//' *.tmhmm
+sed -i 's/^.*PredHel=0.*//' *.tmhmm
 sed -i '/^$/d' *.tmhmm
 
 mkdir tmhmm
 
-python ./pythontmhmmgff.py
+/projects/team2/func_annotation/bin/python3/Python-3.7.2/python ./pythontmhmmgff.py
 
 mv *.tmhmm ./tmhmm
 mv *.gff ./tmhmm
