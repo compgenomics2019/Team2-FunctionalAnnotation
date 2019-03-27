ddf9821adc359c9e4337d929a89c2af0198a8bf8
Neary, Bridget E <bridget.neary@gatech.edu>
2019-03-26 22:37:50 -0400

    Add files via upload

--- ard/ard_run.sh
+++ ard/ard_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-team2=../../team2
+team2=../../../team2
 tools="$team2"/func_annotation/tools
 path_to_resfinder="$tools"/resfinder/resfinder.py
 path_to_dbs="$tools"/arg_dbs
@@ -38,15 +38,15 @@ do
 	file=$(basename "$fasta")
 	[[ v -eq 1 ]] && echo "Running Resfinder for "$(basename "$file")
 	sample=$(echo "$file" | sed 's/_.*//')
-	mkdir -p "$outdir"/resfinder/"$sample"
-	"$path_to_python3" "$path_to_resfinder" \
-		-i "$fasta" \
-		-o "$outdir"/resfinder/"$sample" \
-		-p "$path_to_dbs"/resfinder_db -b "$path_to_blast"n
+	#mkdir -p "$outdir"/resfinder/"$sample"
+	#"$path_to_python3" "$path_to_resfinder" \
+	#	-i "$fasta" \
+	#	-o "$outdir"/resfinder/"$sample" \
+	#	-p "$path_to_dbs"/resfinder_db -b "$path_to_blast"n
 	awk -F "\t" 'BEGIN{OFS="\t"}{
-		print $6, "Resfinder", "ARD", $7, $7, ".", "strand", "frame",
-		"hit_name=", $1, ";accession_no=", $9, ";phenotype=", $8;
-		}' "$outdir"/resfinder/"$sample"/results_tab.txt > "$outdir"/resfinder/"$sample"/almost.gff
+		print $6, "Resfinder", "ARD", $7, $7, ".", ".", ".",
+		"hit_name="$1";accession_no="$9";phenotype="$8;
+		}' "$outdir"/resfinder/"$sample"/results_tab.txt >> "$outdir"/resfinder/"$sample"/almost.gff
 	"$path_to_python3" gffmaker.py \
 		"$outdir"/resfinder/"$sample"/almost.gff \
 		"$path_to_gffs"/"$sample"_contigs.gff \
@@ -65,15 +65,11 @@ mkdir -p "$outdir"/victors
 	-outfmt "6 qseqid sseqid length pident qcovs qstart qend sstart send evalue stitle" \
 	-max_target_seqs 1 \
 	-evalue 0.001
+echo "##gff-version 3" > "$outdir"/victors/cluster_victors.gff
 awk -F "\t" '{ if(($4 >= 90) && ($5 >= 90)) {
-	print } }' "$outdir"/vfdb/vfdb_"$infile".out > "$outdir"/vfdb/vfdb_"$infile"_90.tsv
-awk -F "\t" 'BEGIN{OFS="\t"}{ print $1, "Victors", "ARD", $6, $7, $10, "strand",
-	"frame", "hit_id=", $2, ";hit_name=", $11}' "$outdir"/vfdb/vfdb_"$infile"_90.tsv > "$outdir"/vfdb/almost.gff
-"$path_to_python3" gffmaker.py \
-	"$outdir"/victors/almost.gff \
-	"$path_to_gffs"/"$sample"_contigs.gff \ ####
-	blast \
-	> "$outdir"/victors/"$infile"_victors.gff
+	print } }' "$outdir"/victors/victors_"$infile".out > "$outdir"/victors/victors_"$infile"_90.tsv
+awk -F "\t" 'BEGIN{OFS="\t"}{ print $1, "Victors", "ARD", ".", ".", $10, ".", ".",
+	"hit_id="$2";hit_name="$11}' "$outdir"/victors/victors_"$infile"_90.tsv >> "$outdir"/victors/cluster_victors.gff
 
 [[ v -eq 1 ]] && echo "Querying VFDB"
 mkdir -p "$outdir"/vfdb
@@ -83,25 +79,17 @@ mkdir -p "$outdir"/vfdb
 	-outfmt "6 qseqid sseqid length pident qcovs qstart qend sstart send evalue stitle" \
 	-max_target_seqs 1 \
 	-evalue 0.001
+echo "##gff-version 3" > "$outdir"/vfdb/cluster_vfdb.gff
 awk -F "\t" '{ if(($4 >= 90) && ($5 >= 90)) {
 	print } }' "$outdir"/vfdb/vfdb_"$infile".out > "$outdir"/vfdb/vfdb_"$infile"_90.tsv
-awk -F "\t" 'BEGIN{OFS="\t"}{ print $1, "VFDB", "Virulence Factors", $6, $7, $10, "strand",
-	"frame", "hit_id=", $2, ";hit_name=", $11}' "$outdir"/vfdb/vfdb_"$infile"_90.tsv > "$outdir"/vfdb/almost.gff
-"$path_to_python3" gffmaker.py \
-	"$outdir"/vfdb/almost.gff \
-	"$path_to_gffs"/"$sample"_contigs.gff \ ####
-	blast \
-	> "$outdir"/vfdb/"$infile"_vfdb.gff
+awk -F "\t" 'BEGIN{OFS="\t"}{ print $1, "VFDB", "Virulence Factors", ".", ".", $10, ".", ".",
+	"hit_id="$2";hit_name="$11}' "$outdir"/vfdb/vfdb_"$infile"_90.tsv >> "$outdir"/vfdb/cluster_vfdb.gff
 
 [[ v -eq 1 ]] && echo "Running RGI"
 mkdir -p "$outdir"/rgi
 "$tools"/rgi main -i "$path_to_clusters" -o "$outdir"/rgi/rgi_"$infile" -t protein --clean
 sed 's/;/,/g' "$outdir"/rgi/rgi_"$infile".txt > "$outdir"/rgi/rgi_"$infile"_nosemi.txt
-awk -F "\t" 'BEGIN{OFS="\t"}{ print $1, "RGI", "ARD", "start", "end", ".", "strand",
-	"frame", "hit_name=", $1, ";ARO_accession=", $11, ";drug_class=", $15,
-	";gene_family=", $17}' "$outdir"/rgi/rgi_"$infile"_nosemi.txt > "$outdir"/rgi/almost.gff
-"$path_to_python3" gffmaker.py \
-	"$outdir"/rgi/almost.gff \
-	"$path_to_gffs"/"$sample"_contigs.gff \ ####
-	rgi \
-	> "$outdir"/rgi/"$infile"_rgi.gff
+echo "##gff-version 3" > "$outdir"/rgi/cluster_rgi.gff
+awk -F "\t" 'BEGIN{OFS="\t"}{ print $1, "RGI", "ARD", "start", "end", ".", "strand", "frame", 
+	"hit_name="$1";ARO_accession="$11";drug_class="$15";gene_family="$17;
+	}' "$outdir"/rgi/rgi_"$infile"_nosemi.txt >> "$outdir"/rgi/cluster_rgi.gff
