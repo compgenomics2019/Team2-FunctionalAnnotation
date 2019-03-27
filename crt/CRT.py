051570175c2d923aed3c6ef951e96a5b67bae032
Cao, Mingming <mcao42@gatech.edu>
2019-03-27 03:57:39 -0400

    Update CRT.py

--- crt/CRT.py
+++ crt/CRT.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-import sys
-import subprocess
-from os import listdr
-from os.path import isfile, join
+def crt_act(input_dir)
+  import sys
+  import subprocess
+  from os import listdr
+  from os.path import isfile, join
 
-inputDir = sys.argv[1]
+  inputDir = input_dir
 
-inputfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
-for file1 in inputfiles:
-  if "fasta" in file1:
-    ofile = file1.split(".")[0]+"_crt.out"
-    #print(ofile)
-    subprocess.call(['java', '-cp', '../bin/CRT1.2-CLI.jar', 'crt', file1, ofile])
+  inputfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
+  for file1 in inputfiles:
+    if "fasta" in file1:
+      ofile = file1.split(".")[0]+"_crt.out"
+      #print(ofile)
+      subprocess.call(['java', '-cp', '../bin/CRT1.2-CLI.jar', 'crt', file1, ofile])
 #print(inputfiles)
