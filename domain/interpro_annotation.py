eeb92f4ae117a6a6274ba40596dec4683d6b8f4c
Cao, Mingming <mcao42@gatech.edu>
2019-03-26 22:57:02 -0400

    Update interpro_annotation.py

--- domain/interpro_annotation.py
+++ domain/interpro_annotation.py
@@ -12,7 +12,7 @@ def annotation_interproscan(input_file,output_file):
     process = Popen(args = ['interproscan.sh',   
                             '-i',input_file,    
                             '-f','gff3',     # hmmer method can be replaced
-                            '-o','-dp',output_file],
+                            '-o',output_file,'-dp'],
                             stdout = PIPE, stderr = PIPE)
     stdout, stderr = process.communicate()
     print(stdout,stderr)
