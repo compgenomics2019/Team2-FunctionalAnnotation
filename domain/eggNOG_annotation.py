981bd5cf7f8d3515b30bef8fc399de604f0d98c3
Cao, Mingming <mcao42@gatech.edu>
2019-03-27 00:16:35 -0400

    Update eggNOG_annotation.py

--- domain/eggNOG_annotation.py
+++ domain/eggNOG_annotation.py
@@ -1,7 +1,6 @@
 #!usr/bin/python 3
 
-import sys 
-from subprocess import Popen, PIPE
+import sys,os
 
 # Usage: eggNOG_annotation <input_file> <output_file>
 
@@ -9,27 +8,15 @@ def download_eggnog_database():
 
     # Before annotation, bactNOG database will be checked and installed
 
-    process = Popen(args = ['python',           # This require python 2.7
-                            '/projects/team2/func_annotation/tools/eggnog/eggnog_dataset/eggnog-mapper-1.0.3/download_eggnog_data.py', 
-                            '-y',
-                            'bact'],            # Download specific database for bacteria
-                            stdout = PIPE, stderr = PIPE)
-    stdout, stderr = process.communicate()
-    del stdout,stderr
+    print("Chekcing the eggnog database")
+    os.system("python /projects/team2/func_annotation/tools/eggnog/eggnog_dataset/eggnog-mapper-1.0.3/download_eggnog_data.py -y bact")
+
 
 def annotation_eggnog(input_file,output_file):
     
     # Using diamond for fast and accurate annotation
-
-    process = Popen(args = ['python',           # This require python 2.7
-                            '/projects/team2/func_annotation/tools/eggnog/eggnog_dataset/eggnog-mapper-1.0.3/emapper.py', 
-                            '-i',input_file,    
-                            '-m','diamond',     # hmmer method can be replaced
-                            '-o',output_file],
-                            stdout = PIPE, stderr = PIPE)
-    stdout, stderr = process.communicate()
-    print(stdout,stderr)
-    del stdout,stderr
+    print("Mapping to eggnog")
+    os.system("python /projects/team2/func_annotation/tools/eggnog/eggnog_dataset/eggnog-mapper-1.0.3/emapper.py -i " + input_file + " -m diamond -o " + output_file)
 
 def eggnog_act(intput,output):
     input_file = input
