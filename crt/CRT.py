bc3f0b4f652bd2dd7a4c4c010a66383314070b43
Cao, Mingming <mcao42@gatech.edu>
2019-03-27 06:41:46 -0400

    Update CRT.py

--- crt/CRT.py
+++ crt/CRT.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
 def crt_act(input_dir):
-  import sys
-  import subprocess
-  from os import listdir
-  from os.path import isfile, join
+    # Use aseemblied_genomes
+    import sys
+    import subprocess
+    from os import listdir
+    from os.path import isfile, join
 
-  inputDir = input_dir
-
-  inputfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
-  for file1 in inputfiles:
-    if "fasta" in file1:
-      ofile = file1.split(".")[0]+"_crt.out"
-      #print(ofile)
-      subprocess.call(['java', '-cp', '../bin/CRT1.2-CLI.jar', 'crt', file1, ofile])
-#print(inputfiles)
+    inputDir = input_dir
+    print(inputDir)
+    inputfiles = [f for f in listdir(inputDir) if isfile(join(inputDir, f))]
+    print(inputfiles)
+    for file1 in inputfiles:
+        if "fasta" in file1:
+            ofile = file1.split(".")[0]+"_crt.out"
+            file1 = input_dir  + file1
+            subprocess.call(['java', '-cp', '/projects/team2/func_annotation/bin/crt/CRISPRleader/bin/CRT1.2-CLI.jar', 'crt', file1, ofile])
